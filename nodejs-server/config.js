53cf7e99273765d06030a3cfb69be2b6fd9598a4
Ryan Leulmi <nazim@ryanleulmi.com>
2019-07-18 22:10:48 -0700

    changed git ignore

--- nodejs-server/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    email: "nazim@ryanleulmi.com",
-    password: "hellarmy123",
-    logo: "https://www.themoviedb.org/assets/2/v4/logos/312x276-primary-green-74212f6247252a023be0f02a5a45794925c3689117da9d20ffe47742a665c518.png"
-}

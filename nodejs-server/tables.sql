c07ce49f4631ba8a74fefbaf5a1f40384a7278ce
tux <tux@buster>
2019-09-17 00:15:24 -0700

    switch

--- nodejs-server/tables.sql
+++ nodejs-server/tables.sql
@@ -3,14 +3,32 @@ CREATE TABLE users(
     email VARCHAR(150) NOT NULL PRIMARY KEY UNIQUE,
     password VARCHAR(150) NOT NULL,
     token VARCHAR(150) NOT NULL, -- confirmation token
-    confirmed TINYINT DEFAULT "0",
-    first_login TINYINT DEFAULT "1",
-    plan VARCHAR(10) NOT NULL DEFAULT 'free'
+    confirmed TINYINT DEFAULT 0, 
+    firstLogin TINYINT DEFAULT 1,
+    plan TINYINT NOT NULL DEFAULT 0,
+    subscription VARCHAR(150) DEFAULT "free" -- stripe subscription id
 );
 CREATE TABLE sessions(
+    token VARCHAR(150) NOT NULL PRIMARY KEY UNIQUE,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    token VARCHAR(150) NOT NULL UNIQUE,
     email VARCHAR(150) NOT NULL,
     FOREIGN KEY (email) REFERENCES users(email)
     ON DELETE CASCADE ON UPDATE CASCADE
 );
+CREATE TABLE movies(
+    title VARCHAR(150) NOT NULL PRIMARY KEY UNIQUE,
+    releaseDate VARCHAR(150) NOT NULL,
+    poster VARCHAR(150) NOT NULL,
+    rating INT NOT NULL DEFAULT 0,
+);
+-- Junction / Joining table for a many to many relationship
+CREATE TABLE user_movies(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
+    user VARCHAR(150) NOT NULL,
+    movie VARCHAR(150) NOT NULL,
+    FOREIGN KEY (user) REFERENCES users(email)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (movie) REFERENCES movies(title)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
